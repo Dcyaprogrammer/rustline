246c8ca4cd16a9f45f72dacda02dc161b0a95143
Dcyaprogrammer <dujustin6@gmail.com>
2025-08-04 23:59:13 +0800

    gpt

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
-use std::collections::{BTreeSet, HashMap};
+use std::{collections::{BTreeSet, HashMap}, hash::Hash};
+
+
+pub struct Tokenizer{
+    stoi: HashMap<char, i32>,
+    itos: HashMap<i32, char>,
+}
+
+impl Tokenizer {
+    pub fn from_text(text: &str) -> Self{
+        let chars = text.chars().collect::<BTreeSet<char>>();
+
+        let stoi = chars.
+            iter().
+            enumerate().
+            map(|(i, ch)|(*ch, i as i32)).
+            collect::<HashMap<char, i32>>();
+
+        let itos = chars.
+            iter().
+            enumerate().
+            map(|(i, ch)|(i as i32, *ch)).
+            collect::<HashMap<i32, char>>(); 
+        
+        Self {stoi, itos}
+    }
+
+    pub fn encode(&self, text: &str) -> Vec<i32>{
+        text.chars()
+            .map(|ch| *self.stoi.get(&ch).unwrap())
+            .collect()
+    }
+
+    pub fn decode(&self, tokens: &[i32]) -> String{
+        tokens.iter().map(|i| *self.itos.get(&i).unwrap()).collect()
+    }
+
+}
 
 fn main(){
     let text = include_str!("input.txt");
     println!("Text chars len: {}", text.chars().count());
 
-    let chars = text.chars().collect::<BTreeSet<char>>();
 
-    let stoi = chars.
-        iter().
-        enumerate().
-        map(|(i, ch)|(*ch, i as i32)).
-        collect::<HashMap<char, i32>>();
-
-    println!("{stoi:?}");
 }
